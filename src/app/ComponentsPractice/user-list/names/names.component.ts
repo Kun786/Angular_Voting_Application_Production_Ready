2553324bc39c8a0c601cec9e820e4294776b3e87
fazi1live <faziislive@gmail.com>
2021-10-09 02:50:40 +0500

    commit

--- src/app/ComponentsPractice/user-list/names/names.component.ts
+++ src/app/ComponentsPractice/user-list/names/names.component.ts
@@ -9,10 +9,9 @@ import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/cor
 export class NamesComponent implements OnInit, OnChanges {
   @Input() OutgoingValueToChildComponent: any;
 
-  _GetHighestPoints=27;
+  _GetHighestPoints: Number = 0;
   _Data: any;
   _TemporaryArray: any = [];
-  _LocalStorageData: any;
   constructor(private _LocalStorageService: LocalStorageService) { }
   ngOnInit(): void {
     this.processArticle(this.OutgoingValueToChildComponent);
@@ -26,9 +25,9 @@ export class NamesComponent implements OnInit, OnChanges {
   //Adding Article To Local Storage
   private processArticle(data: any): void {
     if (data != null) {
-      this._LocalStorageData = this._LocalStorageService.GetLocalStorageForArticleData();
-      if (this._LocalStorageData != null) {
-        this._LocalStorageData.map((_KeyPair: any) => {
+      let _LocalStorageData = this._LocalStorageService.GetLocalStorageForArticleData();
+      if (_LocalStorageData != null) {
+        _LocalStorageData.map((_KeyPair: any) => {
           this._TemporaryArray.push({
             _Id: _KeyPair._Id,
             Title: _KeyPair.Title,
@@ -46,6 +45,7 @@ export class NamesComponent implements OnInit, OnChanges {
       this._TemporaryArray = [];
     } else {
       this._Data = this._LocalStorageService.GetLocalStorageForArticleData();
+      this._GetHighestPoints = this._Data[0]._Points;
     }
   }
   //Adding Article To Local Storage
@@ -53,49 +53,76 @@ export class NamesComponent implements OnInit, OnChanges {
 
   //Voting to UpVote
   UpVote(_Id: any) {
-    this._LocalStorageData = this._LocalStorageService.GetLocalStorageForArticleData();
-    this._LocalStorageData.map((_KeyPair: any) => {
-      if (_KeyPair._Id === _Id) {
-        _KeyPair._Points++;
-        let _SortedArray = this._LocalStorageData.sort((a:any, b:any) => (b._Points) - (a._Points));
-        console.log(_SortedArray);
-        this._GetHighestPoints = Math.max(this._LocalStorageData.map((o:any) => o._Points), 1);
-        console.log(this._GetHighestPoints);
-        this._LocalStorageService.SetLocalStorageForArticleData(this._LocalStorageData);
-        this.OutgoingValueToChildComponent = null;
-        this.ngOnInit();
-      }
-    })
+    let _LocalStorageData = this._LocalStorageService.GetLocalStorageForArticleData();
+    console.log( _LocalStorageData);
+    let _SortedItems = _LocalStorageData
+      .map((_KeyPair: any) => {
+        if (_KeyPair._Id === _Id) {
+            _KeyPair._Points++;
+        }
+        return _KeyPair;
+      })
+      .sort((a: any, b: any) => b._Points - a._Points);
+    this._LocalStorageService.SetLocalStorageForArticleData(_SortedItems);
+    this.OutgoingValueToChildComponent = null;
+    this.ngOnInit();
+
+
+
+    // _LocalStorageData.map((_KeyPair: any) => {
+    //   if (_KeyPair._Id === _Id) {
+    //     _KeyPair._Points++;
+    //     let _SortedArray = _LocalStorageData.sort((a:any, b:any) => (b._Points) - (a._Points));
+    //     this._GetHighestPoints = _SortedArray[0]._Points;
+    //     this._LocalStorageService.SetLocalStorageForArticleData(_LocalStorageData);
+    //     this.OutgoingValueToChildComponent = null;
+    //     this.ngOnInit();
+    //   }
+    // })
   }
   //Voting to UpVote
 
 
-  //Voting to DownVote
   DownVote(_Id: any) {
-    this._LocalStorageData = this._LocalStorageService.GetLocalStorageForArticleData();
-    this._LocalStorageData.map((_KeyPair: any) => {
-      if (_KeyPair._Id === _Id) {
-        if (_KeyPair._Points != 0) {
-          _KeyPair._Points--;
-          let _SortedArray = this._LocalStorageData.sort((a:any, b:any) => (b._Points) - (a._Points));
-          console.log(_SortedArray);
-          this._LocalStorageService.SetLocalStorageForArticleData(this._LocalStorageData);
-          this.OutgoingValueToChildComponent = null;
-          this.ngOnInit();
+    console.time('Downvote Starting');
+    let _LocalStorageData = this._LocalStorageService.GetLocalStorageForArticleData();
+    const _SortedItems = _LocalStorageData
+      .map((_KeyPair: any) => {
+        if (_KeyPair._Id === _Id) {
+          if (_KeyPair._Points != 0) {
+            _KeyPair._Points--;
+          }
         }
-      }
-    })
+        return _KeyPair;
+      })
+      .sort((a: any, b: any) => b._Points - a._Points);
+    this._LocalStorageService.SetLocalStorageForArticleData(_SortedItems);
+    this.OutgoingValueToChildComponent = null;
+    this.ngOnInit();
+    console.timeEnd('Downvote Starting');
+    // _LocalStorageData = this._LocalStorageService.GetLocalStorageForArticleData();
+    // _LocalStorageData.map((_KeyPair: any) => {
+    //   if (_KeyPair._Id === _Id) {
+    //     if (_KeyPair._Points != 0) {
+    //       _KeyPair._Points--;
+    //       _LocalStorageData.sort((a:any, b:any) => (b._Points) - (a._Points));
+    //       this._LocalStorageService.SetLocalStorageForArticleData(_LocalStorageData);
+    //       this.OutgoingValueToChildComponent = null;
+    //       this.ngOnInit();
+    //     }
+    //   }
+    // })
   }
   //Voting to DownVote
 
 
   //Delete Article Card From Local Storage
   Delete(_Id: any) {
-    this._LocalStorageData = this._LocalStorageService.GetLocalStorageForArticleData();
-    if (this._LocalStorageData.length > 1) {
-      this._LocalStorageData.map((_KeyPair: any) => {
+    let _LocalStorageData = this._LocalStorageService.GetLocalStorageForArticleData();
+    if (_LocalStorageData.length > 1) {
+      _LocalStorageData.map((_KeyPair: any) => {
         if (_KeyPair._Id === _Id) {
-          let _LocalStorageToUpdate = this._LocalStorageData.filter((_KeyPair: any) => _KeyPair._Id != _Id);
+          let _LocalStorageToUpdate = _LocalStorageData.filter((_KeyPair: any) => _KeyPair._Id != _Id);
           this._LocalStorageService.SetLocalStorageForArticleData(_LocalStorageToUpdate);
         } else {
           void (0);
